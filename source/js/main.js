4dbe88f89808e88cd67b4375241fb847be708f46
Радик <a-radik-87@mail.ru>
2023-03-14 13:43:43 +0300

    Добавляет вариант зацикливания фокуса на попапе

--- source/js/main.js
+++ source/js/main.js
@@ -131,7 +131,37 @@ window.addEventListener('DOMContentLoaded', () => {
     const openPopup = function () {
       callBackPopup.classList.add('is-active');
       body.classList.add('body-lock');
-      input.focus();
+
+      const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+      const focusableElements = callBackPopup.querySelectorAll(focusableElementsString);
+
+      focusableElements = Array.prototype.slice.call(focusableElements);
+
+      const firstTabStop = focusableElements[0];
+      const lastTabStop = focusableElements[focusableElements.length - 1];
+
+      setTimeout(function () {
+        input.focus();
+      }, 1000);
+
+      document.addEventListener('keydown', function (evt) {
+        if (evt.keyCode === 9) {
+
+          if (evt.shiftKey) {
+
+            if (document.activeElement === firstTabStop) {
+              evt.preventDefault();
+              lastTabStop.focus();
+            }
+          } else {
+            if (document.activeElement === lastTabStop) {
+              evt.preventDefault();
+              firstTabStop.focus();
+            }
+          }
+        }
+      });
+
       document.addEventListener('keydown', onEscButtonPress);
     };
 
